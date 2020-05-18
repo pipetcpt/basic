2fb691fc7828dc6b8dc23e7f0f7c33b501bf753e
Sungpil Han <pil.han@gmail.com>
2020-05-18 17:27:53 +0900

    update: manuscript updates

---
+++ fig-tab.R
@@ -0,0 +1,18 @@
+readxl::read_excel("data-raw/fig-tab.xlsx", sheet='16', range="B2:D12")
+
+readxl::read_excel("data-raw/fig-tab.xlsx", sheet='22', range="B2:H6")
+
+readxl::read_excel("data-raw/fig-tab.xlsx", sheet='28', range="B2:E23")
+
+readxl::read_excel("data-raw/fig-tab.xlsx", sheet='47', range="B2:D39")
+
+readxl::read_excel("data-raw/fig-tab.xlsx", sheet="50", range="B2:E36")
+
+readxl::read_excel("data-raw/fig-tab.xlsx", sheet="71", range="B2:F20")
+
+readxl::read_excel("data-raw/fig-tab.xlsx", sheet="112", range="B2:E7")
+
+readxl::read_excel("data-raw/fig-tab.xlsx", sheet="125", range="B2:C6")
+
+readxl::read_excel("data-raw/fig-tab.xlsx", sheet="187", range="B2:F6")
+
---
+++ setup.R
@@ -0,0 +1,12 @@
+# before_chapter_script: ["setup.R"]
+
+library(dplyr)
+library(knitr)
+library(readxl)
+
+knitr::opts_chunk$set(echo=FALSE, 
+                      message = FALSE, 
+                      out.width="100%", 
+                      dpi = 96, 
+                      #fig.width = 1,
+                      error = TRUE)
