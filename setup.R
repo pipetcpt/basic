f686a2777671a2836e328eb4a4f9962b5ddbdfd3
Sungpil Han <pil.han@gmail.com>
2020-05-19 18:50:37 +0900

    update: clean

--- setup.R
+++ setup.R
@@ -1,11 +1,11 @@
 # before_chapter_script: ["setup.R"]
 
-library(dplyr)
-library(knitr)
-library(readxl)
+my_packages <- c('kableExtra', 'readxl', 'readr', 'knitr', 'tidyverse')
+pacman::p_load(char = my_packages)
 
 knitr::opts_chunk$set(echo=FALSE, 
                       message = FALSE, 
+                      warning = FALSE,
                       out.width="100%", 
                       dpi = 96, 
                       #fig.width = 1,
