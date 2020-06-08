06f605f3af6f39b16f9a45c7f037f3da4fde3bf2
Sungpil Han <pil.han@gmail.com>
2020-06-08 18:27:26 +0900

    update: gitbook and pdf

--- _setup.R
+++ _setup.R
@@ -1,17 +1,2 @@
 # before_chapter_script: ["setup.R"]
 
-my_packages <- c('kableExtra', 'readxl', 'readr', 'knitr', 'tidyverse', 'nlme', 'nmw', 'xpose4')
-pacman::p_load(char = my_packages)
-#knitr::write_bib(my_packages, 'references/packages.bib')
-
-opts_chunk$set(echo=FALSE, 
-               message = FALSE, 
-               warning = FALSE,
-               out.width="100%", 
-               dpi = 96, 
-               #fig.width = 1,
-               error = TRUE)
-
-options(knitr.kable.NA = '')
-
-Sys.setenv(RSTUDIO_PDFLATEX = "latexmk")
